af8972dc2918640451c13fa76447f2088054b38c
hcl621 <54358775+hcl621@users.noreply.github.com>
2019-10-08 20:40:53 +0800

    Update 例子-0829-03.算数运算符.py

--- 例子-0829-03.算数运算符.py
+++ 例子-0829-03.算数运算符.py
@@ -8,3 +8,9 @@ print(10%3)	#1
 print(10**3)	#1000
 print(11//3)	#3 不考虑四舍五入
 print（2**0.5）#1.4142135623730951 平方根
+ from math import sqrt
+ a1 = sqrt(9) #开根号
+ print(a5)   
+from random import random
+a6 = random() #产生一个在[0,1)范围内的随机数
+print(a6)
